f74eac6e1bdfd54470fd0f3013b8edb8958142ed
根本貴文 <takafumi@DN52esqa.SUNet>
2022-07-28 16:04:59 -0700

    line api test

--- app.py
+++ app.py
@@ -1,75 +1,30 @@
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
-import random, os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
 app = Flask(__name__)
 
-ques_list = [['T1aN0M0', 'IA1'],
-             ['T1bN0M0', 'IA2'],
-             ['T1cN0M0', 'IA3'],
-             ['T2aN0M0', 'IB'],
-             ['T2bN0M0', 'IIA'],
-             ['T3N0M0', 'IIB'],
-             ['T4N0M0', 'IIIA'],
-             ['T1aN1M0', 'IIB'],
-             ['T1bN1M0', 'IIB'],
-             ['T1cN1M0', 'IIB'],
-             ['T2aN1M0', 'IIB'],
-             ['T2bN1M0', 'IIB'],
-             ['T3N1M0', 'IIIA'],
-             ['T4N1M0', 'IIIA'],
-             ['T1aN2M0', 'IIIA'],
-             ['T1bN2M0', 'IIIA'],
-             ['T1cN2M0', 'IIIA'],
-             ['T2aN2M0', 'IIIA'],
-             ['T2bN2M0', 'IIIA'],
-             ['T3N2M0', 'IIIB'],
-             ['T4N2M0', 'IIIB'],
-             ['T1aN3M0', 'IIIB'],
-             ['T1bN3M0', 'IIIB'],
-             ['T1cN3M0', 'IIIB'],
-             ['T2aN3M0', 'IIIB'],
-             ['T2bN3M0', 'IIIB'],
-             ['T3N3M0', 'IIIC'],
-             ['T4N3M0', 'IIIC'],
-             ['TanyNanyM1a', 'IVA'],
-             ['TanyNanyM1b', 'IVA'],
-             ['TanyNanyM1c', 'IVB']]
+linebot_api = LineBotApi(os.environ["YOUR_CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["YOUR_CHANNEL_SECRET"])
 
-print(len(ques_list))
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
 
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-@app.route('/', methods=["GET"])
-def get():
-    ques_num = random.randrange(len(ques_list))
-    question = ques_list[ques_num][0]
-
-    return render_template('index.html',
-                           title='問題',
-                           message1=question,
-                           ques_num=ques_num)
-
-
-@app.route('/answer', methods=["POST"])
-def post():
-    ans_post = request.form.get('radio')
-    ques_num = int(request.form.get('ques_num'))
-    question = ques_list[ques_num][0]
-    answer = ques_list[ques_num][1]
-
-    if ans_post == answer:
-        result = '○ 正解'
-    else:
-        if ans_post is None:
-            ans_post = '未解答'
-        result = '× 不正解（あなたの解答：{}）'.format(ans_post)
-
-    return render_template('answer.html',
-                           title='解答',
-                           message1=result,
-                           message2=question,
-                           message3=answer)
-
-
+    return 'OK'
+  
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    linebot_api.reply_message(event.reply_token, \
+                             TextSendMessage(text=event.message.text))
+  
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
