88df7b16dc5d186e2f6462e96fd7df9b23104e7a
根本貴文 <takafumi@DN52esqa.SUNet>
2022-07-28 19:37:25 -0700

    line api test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn, MessageAction
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn
 import random, os
 
 app = Flask(__name__)
